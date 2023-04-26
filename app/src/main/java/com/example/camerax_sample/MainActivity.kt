6ace4ab082cca4dca43c72732396032c971c28c6
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2023-04-26 20:56:12 +0900

    CameraXの依存関係追加とDataBinding追加まで

--- app/src/main/java/com/example/camerax_sample/MainActivity.kt
+++ app/src/main/java/com/example/camerax_sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.camerax_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.camerax_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
